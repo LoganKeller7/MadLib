d6f305dcceb5264aa8aed6fd97cc2b0ffc18b9c6
Logan Keller <logan.keller.2023@student.lhschools.org>
2020-02-24 11:59:14 -0500

    Intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,132 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//Phase 1
+
+        String place;
+        String sport;
+        String famousPerson;
+        String actionPastTenseWord;
+        String animal;
+        String genderOfFamousPerson;
+        String season;
+        String car;
+        String state;
+        String food;
+        String store;
+        String noun;
+        String adjective;
+        String color;
+        String aLiquid;
+        String secondColor;
+        String brandOfShoes;
+        String wordEndingInIng;
+        String phoneBrand;
+        String usableNoun;
+        //phase 2
+
+        Scanner keyboard;
+        keyboard = new Scanner (System.in);
+
+        System.out.println("Name a place ");
+        place = keyboard.nextLine();
+
+        System.out.println("Name a sport ");
+        sport = keyboard.nextLine();
+
+        System.out.println("Name a famous person ");
+        famousPerson = keyboard.nextLine();
+
+        System.out.println("Give me a action word in past tense");
+        actionPastTenseWord = keyboard.nextLine();
+
+        System.out.println("Name a animal");
+        animal = keyboard.nextLine();
+
+        System.out.println("What is the gender of the Famous person? he/she");
+        genderOfFamousPerson = keyboard.nextLine();
+
+        System.out.println("Name a Season");
+        season = keyboard.nextLine();
+
+        System.out.println("Name a Car");
+        car = keyboard.nextLine();
+
+        System.out.println("Name a State");
+        state = keyboard.nextLine();
+
+        System.out.println("What's your favorite food?");
+        food = keyboard.nextLine();
+
+        System.out.println("Name a Store");
+        store = keyboard.nextLine();
+
+        System.out.println("Give me a Noun");
+        noun = keyboard.nextLine();
+
+        System.out.println("Give me a Adjective");
+        adjective = keyboard.nextLine();
+
+        System.out.println("Name a Color");
+        color = keyboard.nextLine();
+
+        System.out.println("name a Liquid");
+        aLiquid = keyboard.nextLine();
+
+        System.out.println("Name a Color");
+        secondColor = keyboard.nextLine();
+
+        System.out.println("give me a brand of Shoes");
+        brandOfShoes = keyboard.nextLine();
+
+        System.out.println("Give me a word ending in ing");
+        wordEndingInIng = keyboard.nextLine();
+
+        System.out.println("Name a Phone Brand");
+        phoneBrand = keyboard.nextLine();
+
+        System.out.println("Name something that you would use");
+        usableNoun = keyboard.nextLine();
+
+        //phase 3
+
+        System.out.println(" There once was a person named " + famousPerson + " and " + genderOfFamousPerson + " lived in " + state);
+        System.out.println(genderOfFamousPerson + " was driving in a " + car + " while " + genderOfFamousPerson + " was going to " + store);
+        System.out.println(" to get some " + aLiquid + " , " + brandOfShoes + " , and a " + phoneBrand + " . "+ genderOfFamousPerson + " " + actionPastTenseWord + " because it was " + season);
+        System.out.println(" after that " + genderOfFamousPerson + " wanted to repaint the " + car + " "  + color );
+        System.out.println(genderOfFamousPerson + " didn't like that color so " + genderOfFamousPerson + " got the " + car + " painted " + secondColor);
+        System.out.println(" when " + genderOfFamousPerson + " was driving " + genderOfFamousPerson + " saw a man " + wordEndingInIng);
+        System.out.println(" after that " + genderOfFamousPerson + " wanted to play " + sport + " while playing " + sport + " " + genderOfFamousPerson + " saw " + noun + " and a " + animal);
+        System.out.println(" " + genderOfFamousPerson + " got tired and went to get some " + food + " " + genderOfFamousPerson + " saw someone " + adjective + " and asked what their name was ");
+        System.out.println(" the person said their name and where he was from, he is from " + place + " " +genderOfFamousPerson + " ended the day off with some " + usableNoun);
+
+
+        
+
+
+
+        
+
+
+
+        
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
